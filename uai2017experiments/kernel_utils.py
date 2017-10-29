7922bf658831eeb0268b7511ed0bb899b60b66dc
Sanghack Lee <sanghack.lee@gmail.com>
2017-10-29 10:59:25 -0400

    networkx 2.0 untested

--- uai2017experiments/kernel_utils.py
+++ uai2017experiments/kernel_utils.py
@@ -8,11 +8,11 @@ from pyrcds.domain import RelationalSkeleton, skeleton_to_entities_only_nx_graph
 def shortest_path_kernel_matrix(skeleton: RelationalSkeleton, vertex_kernel_hop: int):
     """a precomputed kernel matrix-based shortest path kernel function given a k-hop neighborhood subgraph"""
     entities_ug = skeleton_to_entities_only_nx_graph(skeleton)
-    entities_ordered = list(entities_ug.nodes())
+    entities_ordered = list(entities_ug.nodes)
     kgraphs = [None] * len(entities_ordered)
     for i, entity in enumerate(entities_ordered):
         reachable = list(nx.single_source_shortest_path_length(entities_ug, entity, vertex_kernel_hop).keys())
-        kgraphs[i] = KGraph(nx.subgraph(entities_ug, reachable), 'item_class', {entity: 'special_label_yo'})
+        kgraphs[i] = KGraph(entities_ug.subgraph(reachable), 'item_class', {entity: 'special_label_yo'})
 
     index_of = {item: index for index, item in enumerate(entities_ordered)}
     VK, _ = labeled_shortest_path_kernel(kgraphs)
